0c13e4d9a02e19b27d05971f942da58ba2ab0433
Yasmina-BRV <yasminal.bravo@hotmail.com>
2024-03-28 10:03:31 +0100

    modify indes.scss, ChatBot.js & list-messages.js

--- src/views/chatbot/list-messages.js
+++ src/views/chatbot/list-messages.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import viewBotMessage from './bot-message';
 import viewUserMessage from './user-message';
 
@@ -7,23 +6,3 @@ export default (messages) => (`
     ${messages.map((message) => (message.type === 'bot' ? viewBotMessage(message) : viewUserMessage(message))).join('')}
   </div>
 `);
-=======
-export default function viewListMessages(messages) {
-  function formatTimestamp(timestamp) {
-    if (timestamp instanceof Date) {
-      return timestamp.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
-    }
-    return timestamp;
-  }
-  return `
-    <div class="messages-container">
-      ${messages.map((msg) => `
-        <div class="${msg.sender === 'Utilisateur' ? 'user-message' : 'bot-message'}">
-          <p>${msg.text}</p>
-          <small>${msg.sender} - ${formatTimestamp(msg.timestamp)}</small>
-        </div>
-      `).join('')}
-    </div>
-  `;
-}
->>>>>>> origin/yasmina
